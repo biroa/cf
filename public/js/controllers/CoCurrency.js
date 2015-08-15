d4a2aa81a7d04f97934dd3e9d29e01f95d6addf1
biroa <adam.biro@gmail.com>
2015-08-15 20:26:32 +0100

    mod Angular currency model and controller

--- public/js/controllers/CoCurrency.js
+++ public/js/controllers/CoCurrency.js
@@ -1,3 +1,25 @@
-App.controller("currencyController", ['$scope', function ($scope) {
-    $scope.currency = ['EUR', 'JEN', 'HUF'];
-}]);
+App.controller("currencyController", [
+    '$scope',
+    'Currency',
+    function ($scope, Currency) {
+
+        $scope.currencies = new Currency();
+        //$scope.currency = ['EUR', 'JEN', 'HUF'];
+        $scope.currency = [];
+        $scope.getData = function ($scope) {
+            $scope.currencyResp = $scope.currencies.$query();
+            $scope.currencyResp.then(function (response) {
+                $scope.currencyData = response.SUCCESS;
+                for (i in $scope.currencyData) {
+                    $scope.currency.push($scope.currencyData[i].currency_code);
+                }
+                //console.log($scope.currency);
+            });
+        };
+
+        if ($scope.currency.length == 0) {
+            $scope.getData($scope);
+        }
+
+
+    }]);
--- public/js/models/MoCurrency.js
+++ public/js/models/MoCurrency.js
@@ -2,10 +2,7 @@
  * Resource Factory for currencies
  */
 Services.factory('Currency', ['$resource', function ($resource) {
-    return $resource('/api/currency/:id', {id: '@id'}, {
-        query: {method: 'GET', isArray: false},
-        get: {method: 'GET', params: {id: '@id'}, isArray: false},
-        save: {method: 'POST', isArray: false},
-        update: {method: 'PUT'}
+    return $resource('/api/currencies/:id', {id: '@id'}, {
+        query: {method: 'GET', isArray: false}
     });
 }]);
